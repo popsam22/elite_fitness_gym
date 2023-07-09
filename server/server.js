ae165642ac3436d0e2d6d4dcfc4f10b71f8dcc97
popsam22 <sobiechie7@gmail.com>
2023-07-09 01:17:53 +0100

    cannot GET/ fix

--- server/server.js
+++ server/server.js
@@ -3,18 +3,27 @@ require("dotenv").config();
 const express = require("express");
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path');
 const workoutRoutes = require('./routes/workouts')
 const userRoutes = require('./routes/user')
 
 const app = express();
 
 //middlewares
+const buildPath = path.join(__dirname, 'client', 'build');
+app.use(express.static(buildPath));
+
 app.use(cors())
 app.use(express.json())
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
 
 //connection to database
 mongoose.connect(process.env.MONGO_URI)
